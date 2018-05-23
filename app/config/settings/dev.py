02b4d7b36ee7e569520296c7b3028ef41a32bb7f
smallbee <smallbee3@gmail.com>
2018-05-24 00:37:28 +0900

    S3 File Storage setting - local: .media / dev,prod: AWS S3 bucket

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -5,4 +5,11 @@ DEBUG = True
 ALLOWED_HOSTS = []
 WSGI_APPLICATION = 'config.wsgi.dev.application'
 
+INSTALLED_APPS += [
+    'storages',
+]
+
+DEFAULT_FILE_STORAGE = 'config.storage.DefaultFilesStorage'
+STATICFILES_STORAGE = 'config.storage.StaticFilesStorage'
+
 import_secrets()
--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -5,4 +5,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 WSGI_APPLICATION = 'config.wsgi.prod.application'
 
+INSTALLED_APPS += [
+    'storages',
+]
+
 import_secrets()
