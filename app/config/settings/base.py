714c4685f338ddfadd09be9393a4b8a7678015e8
unknown <sroyhong313@gmail.com>
2018-05-28 00:27:38 +0900

    add Kakao OAuth authentication

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -66,6 +66,7 @@ AUTH_USER_MODEL = 'users.User'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'users.backends.APIFacebookBackend',
+    'users.backends.APIKakaoBackend'
 ]
 
 REST_FRAMEWORK = {
