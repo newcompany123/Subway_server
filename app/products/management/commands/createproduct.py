ed256c50922d44ed0b02478689f303710868546b
smallbee <smallbee3@gmail.com>
2018-06-03 15:21:46 +0900

    products/management/commands/createproduct.py added

--- app/products/management/commands/createproduct.py
+++ app/products/management/commands/createproduct.py
@@ -1,10 +1,12 @@
 from django.core.management import BaseCommand
 
-from ...models import Vegetables
+from ...models import Vegetables, Breads
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        vegetable_list = ['Tomatoes', 'Lettuce', 'Cucumbers', 'Peppers', 'Red Onions', 'Pickles', 'Olives', 'Jalapenos', 'Avocado']
+        vegetable_list = ['Lettuce', 'Tomatoes', 'Cucumbers', 'Peppers', 'Red Onions', 'Pickles', 'Olives', 'Jalapenos', 'Avocado']
+        bread_list = ['Honey Oat', 'Hearty Italian', 'Wheat', 'Parmesan Oregano', 'White', 'Flat Bread']
 
         [Vegetables.objects.get_or_create(name=name) for name in vegetable_list]
+        [Breads.objects.get_or_create(name=name) for name in bread_list]
