a7783ae934e5fe6ad63afab0359003b46e6a1a5d
smallbee <smallbee3@gmail.com>
2018-06-10 12:42:08 +0900

    ProductName making process added through management.commands.createproduct.py

--- app/products/management/commands/createproduct.py
+++ app/products/management/commands/createproduct.py
@@ -1,12 +1,14 @@
 from django.core.management import BaseCommand
 
-from ...models import Vegetables, Bread
+from ...models import Vegetables, Bread, ProductName
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         vegetable_list = ['Lettuce', 'Tomatoes', 'Cucumbers', 'Peppers', 'Red Onions', 'Pickles', 'Olives', 'Jalapenos', 'Avocado']
         bread_list = ['Honey Oat', 'Hearty Italian', 'Wheat', 'Parmesan Oregano', 'White', 'Flat Bread']
+        productname_list = ['샌드위치이름1', '샌드위치이름2', '샌드위치이름3', '샌드위치이름4', '샌드위치이름5']
 
         [Vegetables.objects.get_or_create(name=name) for name in vegetable_list]
         [Bread.objects.get_or_create(name=name) for name in bread_list]
+        [ProductName.objects.get_or_create(name=name) for name in productname_list]
