9fc2403b40bb99f3e152596ee54362f205949072
smallbee <smallbee3@gmail.com>
2018-06-10 12:18:58 +0900

    User Response modified : Include token inside of User Response

--- app/users/apis/facebook.py
+++ app/users/apis/facebook.py
@@ -14,9 +14,5 @@ class UserFacebookAccessTokenView(APIView):
         serializer = FacebookAccessTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-        token, _ = Token.objects.get_or_create(user=user)
-        data = {
-            'token': token.key,
-            'user': UserSerializer(user).data
-        }
-        return Response(data)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
