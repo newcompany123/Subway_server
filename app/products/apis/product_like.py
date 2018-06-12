fccb00cddddb3144a2ab87219e78ccfa2ce0b50b
smallbee <smallbee3@gmail.com>
2018-06-13 04:11:54 +0900

    get_object_or_404_customed used

--- app/products/apis/product_like.py
+++ app/products/apis/product_like.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from users.serializers import UserSerializer
+from utils.exceptions.get_object_or_404 import get_object_or_404_customed
 from ..models import Product, ProductLike
 
 
@@ -10,7 +11,7 @@ class ProductLikeListCreateView(APIView):
 
     def post(self, request, pk):
         user = request.user
-        product = Product.objects.get(pk=pk)
+        product = get_object_or_404_customed(Product, pk=pk)
         instance, created = ProductLike.objects.get_or_create(
             liker=user,
             product=product,
@@ -19,17 +20,18 @@ class ProductLikeListCreateView(APIView):
         if not created:
             instance.delete()
             return Response(
-                f'User({user})가 product({product})에 대한 좋아요를 취소하였습니다.',
+                f'User(id:{user.pk})가 product(id:{product.pk})에 대한 좋아요를 취소하였습니다.',
                 status=status.HTTP_204_NO_CONTENT,
             )
         else:
             return Response(
-                f'User({user})가 product({product})를 좋아합니다.',
+                f'User(id:{user.pk})가 product(id:{product.pk})를 좋아합니다.',
                 status=status.HTTP_200_OK,
             )
 
     def get(self, request, pk):
-        product = Product.objects.get(pk=pk)
+        # product = Product.objects.get(pk=pk)
+        product = get_object_or_404_customed(Product, pk=pk)
         product_likers = product.product_liker.all()
         serializer = UserSerializer(product_likers, many=True)
         return Response(serializer.data)
--- app/products/apis/product_save.py
+++ app/products/apis/product_save.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from users.serializers import UserSerializer
+from utils.exceptions.get_object_or_404 import get_object_or_404_customed
 from ..models import Product, ProductSave
 
 
@@ -10,7 +11,7 @@ class ProductSaveListCreateView(APIView):
 
     def post(self, request, pk):
         user = request.user
-        product = Product.objects.get(pk=pk)
+        product = get_object_or_404_customed(Product, pk=pk)
         instance, created = ProductSave.objects.get_or_create(
             saver=user,
             product=product,
@@ -19,17 +20,17 @@ class ProductSaveListCreateView(APIView):
         if not created:
             instance.delete()
             return Response(
-                f'User({user})가 product({product})의 저장을 취소했습니다.',
+                f'User(id:{user.pk})가 product(id:{product.pk})의 저장을 취소했습니다.',
                 status=status.HTTP_204_NO_CONTENT,
             )
         else:
             return Response(
-                f'User({user})가 product({product})를 저장했습니다.',
+                f'User(id:{user.pk})가 product(id:{product.pk})를 저장했습니다.',
                 status=status.HTTP_200_OK,
             )
 
     def get(self, request, pk):
-        product = Product.objects.get(pk=pk)
+        product = get_object_or_404_customed(Product, pk=pk)
         product_saver = product.product_liker.all()
         serializer = UserSerializer(product_saver, many=True)
         return Response(serializer.data)
