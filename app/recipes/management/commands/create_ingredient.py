ca93c72dad8ef683faf501f674b47152c3aee9a3
smallbee <smallbee3@gmail.com>
2018-06-25 07:31:15 +0900

    Save imagefile -> imagefile path : To make cache system in mobile client work properly

--- app/recipes/management/commands/create_ingredient.py
+++ app/recipes/management/commands/create_ingredient.py
@@ -40,27 +40,34 @@ class Command(BaseCommand):
             image_path = 'sandwich/' + name + '.jpg'
             static_storage_class = import_string(settings.STATICFILES_STORAGE)
             static_storage = static_storage_class()
-            static_file = static_storage.open(
-                image_path
-            )
 
-            # ------------------------------------------------------------
-            # [ Shoveling log ]
+            # ----------------------------------------
+            # # 1안) ImageField에 imagefile 저장
+            # static_file = static_storage.open(
+            #     image_path
+            # )
+            # # *ImageFileField.save()로 아래처럼 settings에 따른 분기가 필요 없어짐.
+            # # 1) settings = local (Local storage, SQLite)
+            # # if SETTINGS_MODULE == 'config.settings.local':
+            # #     sandwich.image.save(image_path, static_file)
+            # # 2) settings = prod (Amazon RDS, S3)
+            # # else:
+            # #     sandwich.image = static_file
+            # #     sandwich.save()
+            #
+            # sandwich.image.save(image_path, static_file)
+
+            # ----------------------------------------
+            #  2안) image url 경로 저장
             #   : url에 expire가 포함된 hash 같이 붙는 S3의 특성상 file_path만 저장할 수 없고
             #       hash값 자체를 없애는 것도 적절한 방법이라고 보기 힘들기 때문에
             #       저장하는 단계를 추가하더라도 아래의 방법을 선택하게 됨.
-            # image_url = static_storage.url(image_path, expire=30)
-            # sandwich.image = image_url
-            # sandwich.save()
-            # ------------------------------------------------------------
-
-            # ImageFileField.save()로 아래처럼 settings에 따른 분기가 필요 없어짐.
-            # settings = local (Local storage, SQLite)
-            # if SETTINGS_MODULE == 'config.settings.local':
-            #     sandwich.image.save(image_path, static_file)
-            # # settings = prod (Amazon RDS, S3)
-            # else:
-            #     sandwich.image = static_file
-            #     sandwich.save()
-
-            sandwich.image.save(image_path, static_file)
+            #       -> 때문에 위 방법 선택을 안하려고 했는데
+            #          규혁님과 토의후 지속적으로 url에 hash가 붙게되면
+            #          클라이언트(모바일)에서 (기본적으로 하게 되어있는)
+            #          캐싱을 하는 의미가 없이 새로 앱을 작동시킬 때마다
+            #          이미지를 다시 받아오는 문제가 있어서 다시 이 방법으로 회귀
+            #          (* 클라이언트에서 이미지 캐싱은 url 주소로 판단한다고 함)
+            image_url = static_storage.url(image_path)
+            sandwich.image = image_url
+            sandwich.save()
