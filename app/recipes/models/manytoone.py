1bf295e9eb2bb8b7aa313bd382596410f9c6c847
smallbee <smallbee3@gmail.com>
2018-06-25 19:40:29 +0900

    Sandwich model's 'image' field modified : ImageField -> FilePathField

--- app/recipes/models/manytoone.py
+++ app/recipes/models/manytoone.py
@@ -10,8 +10,15 @@ class Sandwich(models.Model):
         unique=True,
         help_text='100자까지 Sandwich의 이름을 저장합니다.'
     )
-    # image = models.FilePathField(path='sandwich', blank=True, max_length=1000)
-    image = models.ImageField(blank=True)
+    image = models.FilePathField(path='sandwich', blank=True, max_length=255)
+
+    # image = models.ImageField(blank=True)
+    # -> ImageField에 image의 url을 저장할 경우 아래와 같은 문제가 발생
+    # (settings == local)
+    # "image": "http://localhost:7000/media/static/sandwich/spicy_italian_avocado.jpg"
+    # (settings == prod)
+    # "image": "https://s3.ap-northeast-2.amazonaws.com/
+    #           bucket-subway/media/https%3A/s3.ap-northeast-2.amazonaws.com/bucket-subway/static/sandwich/spicy_italian_avocado.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIM4DF24YBH6QS24Q%2F20180625%2Fap-northeast-2%2Fs3%2Faws4_request&X-Amz-Date=20180625T102742Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=1aa3b156a1d08c0c66e92fcb04b623bc6ea060bfca4b862c56d1ae82a0635559"
 
     def __str__(self):
         return f'{self.pk})_{self.name}'
