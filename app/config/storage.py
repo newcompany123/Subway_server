2fb90541621c42f1b80f9c91be11d8f993650ee2
smallbee <smallbee3@gmail.com>
2018-06-25 19:37:52 +0900

    Leave out querystring_auth from S3 image url by adding 'querystring_auth = False'

--- app/config/storage.py
+++ app/config/storage.py
@@ -4,6 +4,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticFilesStorage(S3Boto3Storage):
     location = 'static'
     default_acl = 'public-read'
+    querystring_auth = False
 
 
 class DefaultFilesStorage(S3Boto3Storage):
