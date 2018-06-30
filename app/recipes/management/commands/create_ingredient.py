17999c0efd38265f1282b160d9f43f09191f9351
smallbee <smallbee3@gmail.com>
2018-06-30 22:15:07 +0900

    create_ingredient modified for creating MainIngredient objects

--- app/recipes/management/commands/create_ingredient.py
+++ app/recipes/management/commands/create_ingredient.py
@@ -2,27 +2,140 @@ from django.conf import settings
 from django.core.management import BaseCommand
 from django.utils.module_loading import import_string
 
-from ...models import Vegetables, Bread, RecipeName, Sandwich, Cheese, Toppings, Sauces
+from ...models import Vegetables, Bread, RecipeName, Sandwich, Cheese, Toppings, Sauces, MainIngredient
 
 
 class Command(BaseCommand):
+
     def handle(self, *args, **options):
-        recipe_name_list = ['sandwich1', 'sandwich2', 'sandwich3', 'sandwich4', 'sandwich5']
-        sandwich_list = ['spicy_italian_avocado', 'turkey_bacon_avocado', 'veggie_avocado', 'pulled_pork',
-                         'egg_mayo', 'italian_bmt', 'blt', 'meatball', 'ham', 'tuna', 'rotisserie_chicken',
-                         'roasted_chicken', 'roasted_beef', 'subway_club', 'turkey', 'veggie_delite', 'steak_&_cheese',
-                         'chicken_&_bacon_ranch', 'subway_melt', 'turkey_bacon']
-        bread_list = ['honey_oat', 'hearty_italian', 'wheat', 'parmesan_oregano', 'white', 'flat_bread']
-        cheese_list = ['american_cheese', 'shrewd_cheese']
-        vegetable_list = ['lettuce', 'tomatoes', 'cucumbers', 'peppers', 'red_onions', 'pickles', 'olives', 'jalapenos', 'avocado']
-        topping_list = ['double_up', 'egg_mayo', 'omelet']
-        sauce_list = ['ranch', 'mayonnaise', 'sweet_onion']
+        recipe_name_list = ['sandwich1',
+                            'sandwich2',
+                            'sandwich3',
+                            'sandwich4',
+                            'sandwich5',
+                            ]
+
+        sandwich_list = ['b_l_t',
+                         'bacon_egg_cheese',
+                         'black_forest_ham_egg_cheese',
+                         'chicken_bacon_ranch',
+                         'chicken_teriyaki',
+                         'egg_mayo',
+                         'ham',
+                         'italian_b_m_t',
+                         'meatball',
+                         'pulled_pork',
+                         'roasted_beef',
+                         'roasted_chicken',
+                         'rotisserie_chicken',
+                         'spicy_italian_avocado',
+                         'spicy_italian',
+                         'steak_cheese',
+                         'steak_egg_cheese',
+                         'subway_club',
+                         'subway_melt',
+                         'tuna',
+                         'turkey_bacon_avocado',
+                         'turkey_bacon',
+                         'turkey',
+                         'veggie_avocado',
+                         'veggie_delite',
+                         'western_egg_cheese',
+                         ]
+
+        main_ingredients_list = ['all_veggies',
+                                 'chicken_breast',
+                                 'ham',
+                                 'meat_ball',
+                                 'mian_chicken_strip',
+                                 'pepperoni',
+                                 'pulled_pork',
+                                 'roast_beef',
+                                 'rotisserie_chicken',
+                                 'salami',
+                                 'steak',
+                                 'teriyaki',
+                                 'tuna',
+                                 'turkey',
+
+                                 'avocado',     # topping
+                                 'bacon',       # topping
+                                 'egg_mayo',    # topping
+                                 'omelet',      # topping
+
+                                 'american_cheese',   # cheese
+                                 ]
+
+        bread_list = ['flat_bread',
+                      'hearty_italian',
+                      'honey_oat',
+                      'parmesan_oregano',
+                      'wheat',
+                      'white',
+                      ]
+
+        cheese_list = ['american_cheese',
+                       'shrewd_cheese',
+                       ]
+
+        vegetable_list = ['cucumbers',
+                          'jalapenos',
+                          'lettuce',
+                          'olives',
+                          'peppers',
+                          'pickles',
+                          'red_onions',
+                          'tomatoes',
+                          # 'avocado',
+                          ]
+
+        topping_list = ['avocado',
+                        'bacon',
+                        'double_up',
+                        'egg_mayo',
+                        'omelet',
+                        ]
+
+        sauce_list = ['black_pepper',
+                      'chipotle',
+                      'honey_mustard',
+                      'horseradish',
+                      'hot_chilli',
+                      'italian_dressing',
+                      'mayonnaise',
+                      'olive_oil',
+                      'ranch',
+                      'red_wine_vinaigrette',
+                      'salt',
+                      'smoke_bbq',
+                      'sweet_chilli',
+                      'sweet_onion',
+                      'thousand_island',
+                      'yellow_mustard',
+                      ]
+
+        def get_or_create_and_save_images(class_name, folder_name, obj_name_list):
+            for obj_name in obj_name_list:
+                obj, _ = class_name.objects.get_or_create(name=obj_name)
+
+                image_path = folder_name + obj_name + '.png'
+                image_path3x = folder_name + obj_name + '@3x.png'
+
+                static_storage_class = import_string(settings.STATICFILES_STORAGE)
+                static_storage = static_storage_class()
+
+                image_url = static_storage.url(image_path)
+                image_url3x = static_storage.url(image_path3x)
+                obj.image = image_url
+                obj.image3x = image_url3x
+                obj.save()
+
+        get_or_create_and_save_images(MainIngredient, 'main_ingredient/', main_ingredients_list)
 
         [RecipeName.objects.get_or_create(name=name) for name in recipe_name_list]
         # [Sandwich.objects.get_or_create(name=name) for name in sandwich_list]
-        [Cheese.objects.get_or_create(name=name) for name in cheese_list]
-
         [Bread.objects.get_or_create(name=name) for name in bread_list]
+        [Cheese.objects.get_or_create(name=name) for name in cheese_list]
         [Vegetables.objects.get_or_create(name=name) for name in vegetable_list]
         [Toppings.objects.get_or_create(name=name) for name in topping_list]
         [Sauces.objects.get_or_create(name=name) for name in sauce_list]
@@ -30,6 +143,7 @@ class Command(BaseCommand):
         for name in sandwich_list:
             sandwich, _ = Sandwich.objects.get_or_create(name=name)
 
+            # 1) Sandwich 인스턴스 생성 후 image url path 저장
             image_path = 'sandwich/' + name + '.png'
             image_path3x = 'sandwich/' + name + '@3x.png'
 
@@ -69,3 +183,11 @@ class Command(BaseCommand):
             sandwich.image = image_url
             sandwich.image3x = image_url3x
             sandwich.save()
+
+            # 2) Sandwich 인스턴스 생성 후 Main Ingredient 저장
+
+
+
+
+
+        # get_or_create_and_save_images(Sandwich, sandwich_list)
