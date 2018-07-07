96121bcd8b51ce27963a880492d8dcb6a21352a1
smallbee <smallbee3@gmail.com>
2018-07-07 15:42:39 +0900

    filter_backends and CustomFilter added to receive words as a parameters

--- app/recipes/apis/sandwich.py
+++ app/recipes/apis/sandwich.py
@@ -1,8 +1,32 @@
+
+# from django_filters import FilterSet
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet, Filter
+from rest_framework.filters import OrderingFilter
 from rest_framework import generics, permissions
 
+from utils.exceptions.get_object_or_404 import get_object_or_404_customed
 from utils.permission.custom_permission import IsSuperUserOrReadOnly
 from ..serializers.recipe import SandwichSerializer
-from ..models import Sandwich
+from ..models import Sandwich, Category
+
+
+class CustomFilter(Filter):
+    def filter(self, qs, value):
+        if not value:
+            return qs
+
+        obj = get_object_or_404_customed(Category, name=value)
+        return super().filter(qs, obj)
+
+
+class SandwichFilter(FilterSet):
+    category = CustomFilter()
+
+    class Meta:
+        model = Sandwich
+        fields = (
+            'category',
+        )
 
 
 class SandwichListCreateView(generics.ListCreateAPIView):
@@ -13,3 +37,12 @@ class SandwichListCreateView(generics.ListCreateAPIView):
         permissions.IsAuthenticatedOrReadOnly,
         IsSuperUserOrReadOnly,
     )
+
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+
+    # filtering
+    # filter_fields = ('category',)
+    filter_class = SandwichFilter
+
+    # ordering
+    ordering_fields = ('id', 'category_new', 'category_event',)
