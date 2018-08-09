b573f926b185b5dc800040809763a7d5d4a1c9fe
smallbee <smallbee3@gmail.com>
2018-08-09 14:56:14 +0900

    Data caching added to RecipeName API

--- app/recipes/apis/recipe_name.py
+++ app/recipes/apis/recipe_name.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from rest_framework import generics, permissions
 
 from utils.permission.custom_permission import IsSuperUserOrReadOnly
@@ -6,8 +7,11 @@ from ..models import RecipeName
 
 
 class RecipeNameListCreateView(generics.ListCreateAPIView):
-    queryset = RecipeName.objects.all()
+    # queryset = RecipeName.objects.all()
     serializer_class = RecipeNameSerializer
+
+    # Data caching by Redis
+    queryset = cache.get_or_set('recipenames', RecipeName.objects.all().values(), 3600)
 
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly,
