a02ff168f2141fe16fca6efd3b2d86377eff190b
smallbee <smallbee3@gmail.com>
2018-08-09 13:18:39 +0900

    django-redis setting fixed : LOCATION=redis://127.0.0.1:6379/1

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -92,7 +92,7 @@ REST_FRAMEWORK = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:7000/1", # 1번 DB
+        "LOCATION": "redis://127.0.0.1:6379/1", # 1번 DB
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
