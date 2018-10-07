e55bbc6a737732d179c68d5b79466186d3d4f0b9
smallbee <smallbee3@gmail.com>
2018-10-08 01:14:20 +0900

    to_representation removed from BookmarkedRecipeSerializer

--- app/bookmarkcollection/serializers/recipe.py
+++ app/bookmarkcollection/serializers/recipe.py
@@ -10,10 +10,12 @@ class BookmarkedRecipeSerializer(serializers.ModelSerializer):
         model = BookmarkedRecipe
         fields = '__all__'
 
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-
-        data = instance.bookmarkcollection_set.all()
-        serializer = BookmarkCollectionSerializer(data, many=True)
-        ret['collection'] = serializer.data
-        return ret
+    # 특정 유저가 북마크 하는 모든 recipe를 조회하는 화면이므로 collection을 보여줄
+    # 이유가 없음.
+    # def to_representation(self, instance):
+    #     ret = super().to_representation(instance)
+    #
+    #     data = instance.bookmarkcollection_set.all()
+    #     serializer = BookmarkCollectionSerializer(data, many=True)
+    #     ret['collection'] = serializer.data
+    #     return ret
