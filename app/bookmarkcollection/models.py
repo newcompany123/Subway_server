247d355df5d532f9959e92f1372ff1d8082af7e2
smallbee <smallbee3@gmail.com>
2018-10-16 19:06:39 +0900

    unique=True added to 'name' field in model BookmarkCollection

--- app/bookmarkcollection/models.py
+++ app/bookmarkcollection/models.py
@@ -8,10 +8,13 @@ class BookmarkCollection(models.Model):
     """
     Recipes which are Bookmarked by user
     """
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     # bookmarked_recipe = models.ManyToManyField(
     #     Bookmark,
     #     verbose_name='북마크 콜렉션',
     #     blank=True
     # )
+
+    def __str__(self):
+        return f'{self.user}의 북마크콜렉션 {self.name}'
