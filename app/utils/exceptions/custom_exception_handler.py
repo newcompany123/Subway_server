371c05614b605785a91b3c8d7c55dd267bd14f23
smallbee <smallbee3@gmail.com>
2018-10-22 16:45:31 +0900

    custom_exception_handler fixed

--- app/utils/exceptions/custom_exception_handler.py
+++ app/utils/exceptions/custom_exception_handler.py
@@ -7,6 +7,11 @@ def custom_exception_handler(exc, context):
 
     # if response is not None:
     #     response.data['status_code'] = response.status_code
-    if exc.pk:
-        response.data['pk'] = exc.pk
-    return response
+
+    try:
+        if exc.pk:
+            response.data['pk'] = exc.pk
+    # except AttributeError:
+    #     return response
+    finally:
+        return response
