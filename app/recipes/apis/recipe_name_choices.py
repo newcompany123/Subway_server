86caa66d2230b18ae7b58fce182d44f81537dfe0
smallbee <smallbee3@gmail.com>
2018-11-11 13:23:54 +0900

    Validation for duplicate recipe_name added to create_recipe_name_choices

--- app/recipes/apis/recipe_name_choices.py
+++ app/recipes/apis/recipe_name_choices.py
@@ -7,12 +7,7 @@ from utils.create_recipe_name_choices import create_recipe_name_choice
 class RecipeNameChoicesListAPIView(APIView):
 
     def get(self, request):
-
-        recipe_name_choices_list = []
-
-        for i in range(5):
-            choice = create_recipe_name_choice()
-            recipe_name_choices_list.append(choice)
+        recipe_name_choices_list = create_recipe_name_choice()
         print(recipe_name_choices_list)
 
         return Response({'recipe_name_choices_list': recipe_name_choices_list})
