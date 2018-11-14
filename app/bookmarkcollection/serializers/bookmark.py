3483e278c3dc48bbae0dcbd4dfe5366749167f1d
smallbee <smallbee3@gmail.com>
2018-11-14 17:58:58 +0900

    Validation for collection added to BookmarkSerializer (Bookmark PATCH API)

--- app/bookmarkcollection/serializers/bookmark.py
+++ app/bookmarkcollection/serializers/bookmark.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 
 from recipes.models import Bookmark
 from users.serializers import UserSerializer
+from utils.exceptions import CustomAPIException
 
 
 class BookmarkSerializer(serializers.ModelSerializer):
@@ -11,6 +12,18 @@ class BookmarkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bookmark
         fields = '__all__'
+
+    def validate_collection(self, collection):
+
+        user = self.context['request'].user
+
+        if not user.bookmarkcollection_set.filter(id=collection.pk):
+            raise CustomAPIException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='collection does not belong to request user',
+                collectio=collection.pk
+            )
+        return collection
 
     # 특정 유저가 북마크 하는 모든 recipe를 조회하는 화면이므로 collection을 보여줄
     # 이유가 없음.
