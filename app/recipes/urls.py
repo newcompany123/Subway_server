9baad7b7fad0fdedf40a973e9bb14e2b3e2f4fa2
smallbee <smallbee3@gmail.com>
2018-11-29 20:24:36 +0900

    recipes/apis/like renamed as like_toggle

--- app/recipes/urls.py
+++ app/recipes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from recipes.apis.recipe_name_choices import RecipeNameChoicesListAPIView
 from .apis.validation import RecipeValidationAPIView
 from .apis.bookmark_toggle import BookmarkListCreateView
-from .apis.like import LikeListCreateView
+from .apis.like_toggle import LikeListCreateView
 from .apis.recipe import RecipeListCreateView, RecipeRetrieveUpdateDestroyView
 
 urlpatterns = [
